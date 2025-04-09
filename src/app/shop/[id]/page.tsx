20184bb1b32b4bf647165795ddb0bc7e6c8d17ea
Salomon <salomon@MacBook-Air-von-Admin.fritz.box>
2025-04-09 22:42:31 +0200

    Erhöhe Bildcontainer-Höhe auf 720px

--- src/app/shop/[id]/page.tsx
+++ src/app/shop/[id]/page.tsx
@@ -30,7 +30,7 @@ export default function ProductPage({ params }: { params: { id: string } }) {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="relative w-full h-[600px] bg-black rounded-lg overflow-hidden">
+        <div className="relative w-full h-[720px] bg-black rounded-lg overflow-hidden">
           <Image
             src={product.image}
             alt={product.title}
